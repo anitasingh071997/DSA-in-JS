761e8e022f09821a06205b6c643f4faa64b76ca9
Anita Singh <139321787+anitasingh071997@users.noreply.github.com>
2024-10-18 23:46:28 +0530

    Add-a-Vertex

---
+++ Graphs-Adding-a-Vertax.js
@@ -0,0 +1,22 @@
+/* ADDING A VERTEX
+Write a method called addVertex, which
+accepts a name of a vertex.
+
+It should add a key to the adjacency list 
+with the name of the vertex and set its
+value to be an empty array.
+*/
+
+g.addVertex("Tokyo")
+{
+    "Tokyo"; []
+}
+
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+}
