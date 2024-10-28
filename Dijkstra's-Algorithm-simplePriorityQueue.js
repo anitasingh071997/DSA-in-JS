36761b27dea521a9b2c5d4bb505ccec3cd47ce06
Anita Singh <139321787+anitasingh071997@users.noreply.github.com>
2024-10-28 20:44:24 +0530

    simple-priority-queue

---
+++ Dijkstra's-Algorithm-simplePriorityQueue.js
@@ -0,0 +1,17 @@
+class PriorityQueue {
+    constructor(){
+      this.values = [];
+    }
+    enqueue(val, priority) {
+      this.values.push({val, priority});
+      this.sort();
+    };
+    dequeue() {
+      return this.values.shift();
+    };
+    sort() {
+      this.values.sort((a, b) => a.priority - b.priority);
+    };
+  }
+//    Notice we are sorting which is O(N * log(N))
+
